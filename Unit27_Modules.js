48b2783dc21e8b9a8f7ddad353be35e7075f6546
Max Lasitsa <150858712+mlasitsa@users.noreply.github.com>
2024-01-03 15:08:15 -0600

    Create Unit27_Modules.js

---
+++ Unit27_Modules.js
@@ -0,0 +1,38 @@
+/*
+    UNIT 27: MODULES 
+
+    This is will be theory mostly and the 
+    code that I am going to have here won't run 
+
+    In JavaScript, the term "modules" typically refers to a way of 
+    organizing and structuring code into reusable and maintainable units. 
+    In the past, JavaScript did not have built-in support for modules, 
+    but with the introduction of ECMAScript 2015 (ES6), 
+    native support for modules was added. 
+
+    We will be talking specifically about export and import modules
+    I am not sure if you have seen, but in Full-Stack Applications 
+    we have a folder called modules that has different JS files. 
+    This files have some kind of functionality and we can either 
+    export FROM files or import TO files  
+*/
+
+/*
+    For example, lets assume that you have two modules called 
+    moduleOne.js
+    moduleTwo.js  
+*/
+
+// moduleOne.js code
+const myName = () => {
+    console.log('Max');
+}
+
+export default myName;
+
+// We want to use this function in 
+// Our moduleTwo.js, so in the 
+// moduleTwo.js we would put:
+
+import printMyName from './moduleOne.js'  // here in the ' ' we set the directory(location) of the file ./folder/fileName
+printMyName(); // Output: Max. As I said this is just an example and the code won't run here 
